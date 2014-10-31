24ee003a01d354d808c3125f1a96e8a1bc954c7b
Loïc Nogues <nogues.loic@gmail.com>
2014-10-31 17:27:24 +0300

    autocompletion for cities (not perfect yet)

--- client/templates/lifts/lift_submit.js
+++ client/templates/lifts/lift_submit.js
@@ -1,3 +1,20 @@
+Template.liftSubmit.rendered = function() {
+  var from = document.getElementById('from');
+  var to = document.getElementById('to');
+
+  var typeaheadCallback = function(query, callback) {
+    Meteor.call('citySearch', query, function(err, res) {
+      console.log(res);
+      callback(res);
+    });
+  };
+
+  Meteor.typeahead(from, typeaheadCallback);
+  Meteor.typeahead(to, typeaheadCallback);
+
+  Meteor.typeahead.inject();
+};
+
 Template.liftSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
---
+++ server/methods.js
@@ -0,0 +1,29 @@
+Meteor.methods({
+  citySearch: function(query) {
+    check(query, String);
+
+    var uniqueArray = [];
+    var results = [];
+
+    var geo = new GeoCoder({
+      geocoderProvider: "openstreetmap"
+    });
+
+    console.log('init geo');
+    var places = geo.geocode(query + ", France");
+
+    places.forEach(function(place) {
+      if (typeof(place.city) != 'undefined')
+        uniqueArray[place.city] = '';
+    });
+
+    console.log(uniqueArray);
+
+    for (city in uniqueArray)
+      results.push({ value: city });
+
+    console.log(results);
+
+    return results;
+  }
+});
