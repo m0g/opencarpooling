d2d531abcbba705ee01340c557df9ebbeac3da23
Loïc Nogues <nogues.loic@gmail.com>
2014-11-04 17:41:14 +0300

    installed font awesome + slightly better looking inputs

--- client/templates/lifts/lifts_list.js
+++ client/templates/lifts/lifts_list.js
@@ -1,3 +1,21 @@
+Template.liftsList.rendered = function() {
+  var from = document.getElementById('from');
+  var to = document.getElementById('to');
+
+  var typeaheadCallback = function(query, callback) {
+    Meteor.call('citySearch', query, function(err, res) {
+      console.log(res);
+      callback(res);
+    });
+  };
+
+  Meteor.typeahead(from, typeaheadCallback);
+  Meteor.typeahead(to, typeaheadCallback);
+  Meteor.typeahead.inject();
+
+  $('#when').datepicker({ format: 'DD/MM/YYYY' });
+};
+
 Template.liftsList.helpers({
   lifts: function() {
     return Lifts.find({}, {sort: {submitted: -1}});
