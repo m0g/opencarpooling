40ee77a4d224dd9395c06c78bbbb3ea335093c12
Loïc Nogues <nogues.loic@gmail.com>
2014-12-16 17:51:04 +0300

    geospatial query

--- server/publications.js
+++ server/publications.js
@@ -2,14 +2,40 @@ Meteor.publish('lifts', function() {
   return Lifts.find();
 });
 
-Meteor.publish('liftsSearch', function() {
-  var query = { fromLoc: { $near : { 
-    $geometry: { 
-      type : "Point" ,
-      coordinates: [ 6.11667, 45.9 ]
-    } },
-    $maxDistance : 50
-  }};
+Meteor.publish('liftsSearch', function(from, to, when) {
+  check(from, String);
+  check(to, String);
+  check(when, String);
+
+  // Need to get the coords for from & to
+  var fromCoords = CitiesCache.findOne({ query: from }).autoCompletion[0];
+  fromCoords = [ parseFloat(fromCoords.lat), parseFloat(fromCoords.lng) ];
+
+  var toCoords = CitiesCache.findOne({ query: to }).autoCompletion[0];
+  toCoords = [ parseFloat(toCoords.lat), parseFloat(toCoords.lng) ];
+
+  console.log(fromCoords);
+  console.log(toCoords);
+
+  var query = { 
+    fromLoc: { $near : { 
+      $geometry: { 
+        type : "Point" ,
+        coordinates: fromCoords
+      },
+      $maxDistance : 50,
+      $minDistance : 10
+    }},
+
+    toLoc: { $near : { 
+      $geometry: { 
+        type : "Point" ,
+        coordinates: toCoords
+      },
+      $maxDistance : 50,
+      $minDistance : 10
+    }}
+  };
 
   return Lifts.find(query);
 });
--- server/startup.js
+++ server/startup.js
@@ -1,4 +1,5 @@
 if (Lifts.find().count() === 0) {
   // ensureIndex on the server side
-  Lifts._ensureIndex({ fromLoc: "2d" });
+  Lifts._ensureIndex({ fromLoc: "2dsphere" });
+  Lifts._ensureIndex({ toLoc: "2dsphere" });
 } 
