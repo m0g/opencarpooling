37a279e1e88f64569ae8562e744090d02604f486
Loïc Nogues <nogues.loic@gmail.com>
2014-12-20 17:26:23 +0300

    i18n support up & running, with french defined as the only and default language

--- client/startup.js
+++ client/startup.js
@@ -1,3 +1,20 @@
+var getUserLanguage = function () {
+  // Put here the logic for determining the user language
+
+  return "fr";
+};
+
 Meteor.startup(function() {
   L.mapbox.accessToken = Meteor.settings.public.mapboxApiKey;
+
+  Session.set("showLoadingIndicator", true);
+
+    TAPi18n.setLanguage(getUserLanguage())
+      .done(function () {
+        Session.set("showLoadingIndicator", false);
+      })
+      .fail(function (error_message) {
+        // Handle the situation
+        console.log(error_message);
+      });
 });
