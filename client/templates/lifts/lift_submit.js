c455c03d2f5e51e8176271d4fb50463af31e5c0e
Loïc Nogues <nogues.loic@gmail.com>
2015-01-23 16:44:58 +0300

    fixed the local storage

--- client/templates/lifts/lift_submit.js
+++ client/templates/lifts/lift_submit.js
@@ -3,13 +3,51 @@ var map = null;
 var checkLocalStorage = function() {
   var lift = lscache.get('liftSubmit');
 
-  console.log(lift);
   if (!lift) return false;
 
   $('form.main.form').find('[name=from]').val(lift.from);
+  $('form.main.form').find('[name=from-lat]').val(lift.fromLat);
+  $('form.main.form').find('[name=from-lng]').val(lift.fromLng);
+
   $('form.main.form').find('[name=to]').val(lift.to);
+  $('form.main.form').find('[name=to-lat]').val(lift.toLat);
+  $('form.main.form').find('[name=to-lng]').val(lift.toLng);
+
   $('form.main.form').find('[name=date]').val(lift.date);
   $('form.main.form').find('[name=price]').val(lift.price);
+
+  if (lift.from.length > 0 && lift.to.length > 0) inputChanged();
+},
+
+inputChanged = function(e) {
+  var markerFromLat = parseFloat(document.getElementById('from-lat').value);
+  var markerFromLng = parseFloat(document.getElementById('from-lng').value);
+  var markerToLat = parseFloat(document.getElementById('to-lat').value);
+  var markerToLng = parseFloat(document.getElementById('to-lng').value);
+
+  if (markerFromLat && markerFromLng && markerToLat && markerToLng)
+    map.mapDirection(markerFromLat, markerFromLng, markerToLat, markerToLng);
+},
+
+storeLocally = function(e) {
+  var cachedLift = lscache.get('liftSubmit');
+
+  console.log(cachedLift);
+
+  var lift = {
+    from: $('form.main.form').find('[name=from]').val() || (cachedLift && cachedLift.from),
+    fromLat: $('form.main.form').find('[name=from-lat]').val() || (cachedLift && cachedLift.fromLat),
+    fromLng: $('form.main.form').find('[name=from-lng]').val() || (cachedLift && cachedLift.fromLng),
+
+    to: $('form.main.form').find('[name=to]').val() || (cachedLift && cachedLift.to),
+    toLat: $('form.main.form').find('[name=to-lat]').val() || (cachedLift && cachedLift.toLat),
+    toLng: $('form.main.form').find('[name=to-lng]').val() || (cachedLift && cachedLift.toLng),
+
+    date: $('form.main.form').find('[name=date]').val() || (cachedLift && cachedLift.date),
+    price: $('form.main.form').find('[name=price]').val() || (cachedLift && cachedLift.price)
+  };
+
+  lscache.set('liftSubmit', lift, 200);
 };
 
 Template.liftSubmit.rendered = function() {
@@ -28,6 +66,7 @@ Template.liftSubmit.rendered = function() {
       fromLat.value = res[0].lat;
       fromLng.value = res[0].lng;
 
+      storeLocally();
       callback(res);
     });
   });
@@ -39,6 +78,7 @@ Template.liftSubmit.rendered = function() {
       toLat.value = res[0].lat;
       toLng.value = res[0].lng;
 
+      storeLocally();
       callback(res);
     });
   });
@@ -48,44 +88,16 @@ Template.liftSubmit.rendered = function() {
   $('#date').datepicker({ format: 'DD/MM/YYYY' });
   $('#seats').raty({ starType : 'i', scoreName: 'seats' });
 
-  checkLocalStorage();
-
   map = new Mapping('form-map', { deactivateZoom: true, polyline: true });
   map.setAsBackground();
-};
 
-var inputChanged = function(e) {
-  markerFromLat = parseFloat(document.getElementById('from-lat').value);
-  markerFromLng = parseFloat(document.getElementById('from-lng').value);
-  markerToLat = parseFloat(document.getElementById('to-lat').value);
-  markerToLng = parseFloat(document.getElementById('to-lng').value);
-
-  if (markerFromLat && markerFromLng && markerToLat && markerToLng)
-    map.mapDirection(markerFromLat, markerFromLng, markerToLat, markerToLng);
-},
-
-storeLocally = function(e) {
-  var cachedLift = lscache.get('liftSubmit');
-
-  if (!cachedLift) return false;
-
-  var lift = {
-    from: cachedLift.from || $('form.main.form').find('[name=from]').val(),
-    to: cachedLift.to || $('form.main.form').find('[name=to]').val(),
-    date: cachedLift.date || $('form.main.form').find('[name=date]').val(),
-    price: cachedLift.price || $('form.main.form').find('[name=price]').val()
-  };
-
-
-  lscache.set('liftSubmit', lift, 200);
+  checkLocalStorage();
 };
 
 Template.liftSubmit.events({
   'keyup #from': inputChanged,
   'keyup #to': inputChanged,
 
-  'change #from': storeLocally,
-  'change #to': storeLocally,
   'change #date': storeLocally,
   'change #price': storeLocally,
 
@@ -112,6 +124,8 @@ Template.liftSubmit.events({
       if (result.liftExists)
         alert('This link has already been posted');
 
+      // Need to remove the input content too
+      lscache.remove('liftSubmit')
       Router.go('liftPage', {_id: result._id});
     });
   }
