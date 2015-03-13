e49580904ddaa924f03aa91d7100efa5c7a96759
Loïc Nogues <nogues.loic@gmail.com>
2015-03-13 15:44:51 +0300

    fixed the geocoding query

--- server/publications.js
+++ server/publications.js
@@ -23,31 +23,45 @@ Meteor.publish('liftsSearch', function(from, to, when) {
   var toCoords = CitiesCache.findOne({ query: to }).autoCompletion[0];
   toCoords = [ parseFloat(toCoords.lat), parseFloat(toCoords.lng) ];
 
-  console.log(fromCoords);
-  console.log(toCoords);
+  var self = this;
 
-  var query = { $and: [
-    {
-      fromLoc: { $near : { 
-        $geometry: { 
-          type : "Point" ,
-          coordinates: fromCoords
-        },
-        $maxDistance : 50,
-        $minDistance : 10
-      }}
-    },
-    {
-      toLoc: { $near : { 
-        $geometry: { 
-          type : "Point" ,
-          coordinates: toCoords
-        },
-        $maxDistance : 50,
-        $minDistance : 10
-      }}
+  // Prepare the queries
+  var queryFrom = {
+    fromLoc: { $near : { 
+      $geometry: { 
+        type : "Point" ,
+        coordinates: fromCoords
+      },
+      $maxDistance : 50,
+      $minDistance : 0
+    }}
+  },
+
+  queryTo = {
+    toLoc: { $near : { 
+      $geometry: { 
+        type : "Point" ,
+        coordinates: toCoords
+      },
+      $maxDistance : 50,
+      $minDistance : 0
+    }}
+  };
+
+  // geoNear queries don't work with more than one near value.
+  // So we execute two different queries that we will then merge
+  // into one cursor (using the Meteor API).
+  Lifts.find(queryFrom).forEach(function(result) {
+    if (JSON.stringify(result.toLoc) == JSON.stringify(toCoords)) {
+      self.added("lifts", result._id, result);
     }
-  ]};
+  });
 
-  return Lifts.find(query);
+  Lifts.find(queryTo).forEach(function(result) {
+    if (JSON.stringify(result.fromLoc) == JSON.stringify(fromCoords)) {
+      self.added("lifts", result._id, result);
+    }
+  });
+
+  self.ready();
 });
