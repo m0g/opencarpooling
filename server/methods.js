8a70a1411eaa1c0dde623e7f08efe1009b92732c
Loïc Nogues <nogues.loic@gmail.com>
2015-06-01 18:34:57 +0300

    Fixed HTTP.call blocking error but loose latency compensation

--- server/methods.js
+++ server/methods.js
@@ -1,19 +1,38 @@
 Meteor.methods({
-  //citySearch: function(query) {
-  //  check(query, String);
+  citySearch: function(query) {
+    check(query, String);
 
-  //  var citiesCache = CitiesCache.findOne({ query: query });
-  //  console.log(citiesCache);
+    var citiesCache = CitiesCache.findOne({ query: query });
+    console.log(citiesCache);
 
-  //  if (citiesCache) return citiesCache.autoCompletion;
+    if (citiesCache) return citiesCache.autoCompletion;
 
-  //  var geoname = new Geonames();
-  //  var autoCompletion = geoname.getCities(query);
+    var geoname = new Geonames();
+    var autoCompletion = geoname.getCities(query);
 
-  //  var citiesCacheId = CitiesCache.insert({ query: query, autoCompletion: autoCompletion });
+    var citiesCacheId = CitiesCache.insert({ query: query, autoCompletion: autoCompletion });
 
-  //  return autoCompletion;
-  //},
+    return autoCompletion;
+  },
+
+  getCity: function(query) {
+   check(query, String);
+
+    var citiesCache = CitiesCache.findOne({ query: query });
+
+    if (citiesCache)
+      return { value: citiesCache.autoCompletion[0].value,
+               loc: [ parseFloat(citiesCache.autoCompletion[0].lng),
+                      parseFloat(citiesCache.autoCompletion[0].lat) ] };
+
+    var geoname = new Geonames();
+    var autoCompletion = geoname.getCities(query);
+
+    var citiesCacheId = CitiesCache.insert({ query: query, autoCompletion: autoCompletion });
+
+    return { value: autoCompletion[0].value,
+             loc: [ parseFloat(autoCompletion[0].lng), parseFloat(autoCompletion[0].lat)]};
+  },
 
   directionsSearch: function(from, to) {
     check(from, String);
