379eb91a934ba56e1c0c805e0668a64746a7abad
Loïc Nogues <nogues.loic@gmail.com>
2015-06-02 17:39:46 +0300

    Latency compensation for activation code

--- server/methods.js
+++ server/methods.js
@@ -46,6 +46,7 @@ Meteor.methods({
 
     var res = HTTP.get("http://maps.googleapis.com/maps/api/directions/json", options);
 
+    console.log('res data', options, res.data);
     var startLocation = res.data.routes[0].legs[0].start_location
       , duration = res.data.routes[0].legs[0].duration.text
       , distance = res.data.routes[0].legs[0].distance.text;
@@ -65,45 +66,44 @@ Meteor.methods({
     return directionsCache;
   },
 
-  sendActivationCode: function(liftId, email) {
-    check(liftId, String);
-    check(email, String);
+  //sendActivationCode: function(liftId, email) {
+  //  check(liftId, String);
+  //  check(email, String);
 
-    console.log('liftid', liftId);
-    console.log('rand', Random.id());
+  //  console.log('liftid', liftId);
+  //  console.log('rand', Random.id());
 
-    var user = Meteor.users.find({ emails: { $elemMatch: { address: email } } });
-    var lift = Lifts.findOne({ _id: liftId });
+  //  var user = Meteor.users.find({ emails: { $elemMatch: { address: email } } });
+  //  var lift = Lifts.findOne({ _id: liftId });
 
-    if (!user)
-      user = Meteor.users.insert({ username: email, emails: [email] });
+  //  if (!user)
+  //    user = Meteor.users.insert({ username: email, emails: [email] });
 
-    if (lift) {
-      Lifts.update(liftId, { $set: { userId: user.id }});
+  //  if (lift) {
+  //    Lifts.update(liftId, { $set: { userId: user.id }});
 
-      var token = jwt.encode({ liftId: liftId, userId: user.id }, Meteor.settings.secret);
-      var body = [
-        '<a href="http://localhost:3000/activatelift/',
-        token,
-        '">Activate your lift</a>'
-      ].join('');
+  //    var token = jwt.encode({ liftId: liftId, userId: user.id }, Meteor.settings.secret);
+  //    var body = [
+  //      '<a href="http://localhost:3000/activatelift/',
+  //      token,
+  //      '">Activate your lift</a>'
+  //    ].join('');
 
-      var mailSent = Meteor.Mandrill.send({
-        host:           "smtp.mandrillapp.com",
-        port:           587,
-        to:             email,
-        from:           "info@opencarpooling.meteor.com",
-        subject:        'Activation mail',
-        html:           body,
-        authentication: "LOGIN",
-        username:       Meteor.settings.mandrill.username,
-        password:       Meteor.settings.mandrill.password
-      });
+  //    var mailSent = Meteor.Mandrill.send({
+  //      host:           "smtp.mandrillapp.com",
+  //      port:           587,
+  //      to:             email,
+  //      from:           "info@opencarpooling.meteor.com",
+  //      subject:        'Activation mail',
+  //      html:           body,
+  //      authentication: "LOGIN",
+  //      username:       Meteor.settings.mandrill.username,
+  //      password:       Meteor.settings.mandrill.password
+  //    });
 
-      return { liftId: liftId };
-      //return { liftId: liftId, user: user };
-    }
-  },
+  //    return { liftId: liftId };
+  //  }
+  //},
 
   liftActivate: function(token) {
     check(token, String);
